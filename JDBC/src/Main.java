759a8bbf4574d98caabb0a15dd57f8b6fc5117b0
Anuj Vidhate <84793455+Anuj7th-Hokage@users.noreply.github.com>
2024-12-20 19:25:00 +0530

    Advance java

---
+++ JDBC/src/Main.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver is loaded");
+
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mydb", "root", "9890099723");
+            System.out.println("Connected to the database!");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT id, name, age, salary FROM emp1");
+
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getInt(3)+" "+resultSet.getInt(4));
+
+            }
+
+        } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
+
